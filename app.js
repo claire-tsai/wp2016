8cdd54ccf6cd044e2e531411e48bb9a6b9af0846
claire-tsai <qs2536@gmail.com>
2016-10-30 17:59:39 +0800

    click

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="jquery.mousewheel.min.js"></script>
-$('p').click(function(){
-  $(this).css('font','blue');
+$('button').click(function(){
+  $('p').toggle();
 });
+
+
+
+
 
 
 
